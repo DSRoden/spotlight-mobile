c116a52d1e0ea28081c8fc76f52c1937ed71f761
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-17 12:59:39 -0600

    global chat connecting between heroku server and mobile

--- www/js/values/config.js
+++ www/js/values/config.js
@@ -1,8 +1,9 @@
 (function(){
     'use strict';
     angular.module('starter')
-    .value('origin', 'http://localhost:8080')
-    .value('socketUrl', 'http://localhost:8080');
+    .value('origin', 'https://damp-depths-3640.herokuapp.com')
+    .value('socketUrl', 'https://damp-depths-3640.herokuapp.com');
 })();
 
 // heroku url:   https://damp-depths-3640.herokuapp.com
+// local url: http://localhost:8080
