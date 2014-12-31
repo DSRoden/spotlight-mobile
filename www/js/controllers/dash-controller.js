a889a20c1eaa64f4493b54bac0d1ba225d232fa8
DSRoden <Daniel.S.Roden@gmail.com>
2014-12-31 10:36:31 -0500

    photos auth

--- www/js/controllers/dash-controller.js
+++ www/js/controllers/dash-controller.js
@@ -41,9 +41,10 @@
       } else {
 
         //make a call to db to get all photos for current day
-        // Photo.getAllAuthenticated().then(function(response){
-        //   $scope.photos = response.data;
-        // });
+        Photo.getAllAuthenticated().then(function(response){
+          $scope.photos = response.data;
+        });
+
         console.log('authenticated');
         //make a call to db to get all messages for current day
         Message.getAllAuthenticated().then(function(response){
